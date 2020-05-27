ed515d007cc483c7a8b16b9368be4aa495af3ce6
david <david@DESKTOP-KSMVEUL>
2020-05-27 00:43:18 -0600

    Proyecto

---
+++ ReservaDeVuelos/ReservaDeVuelos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReservaDeVuelos.MvcApplication" Language="C#" %>
---
+++ ReservaDeVuelos/ReservaDeVuelos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReservaDeVuelos.Startup))]
+namespace ReservaDeVuelos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
