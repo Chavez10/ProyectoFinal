40e7390f2bc70479a0dda4aef09b4edd4200e48f
HP <HP@DESKTOP-TIIAHDB>
2020-05-28 00:18:07 -0600

    comienzo de login

---
+++ ReservaDeVuelos/ReservaDeVuelos/Controllers/AccesoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReservaDeVuelos.Models;
+
+namespace ReservaDeVuelos.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string pass)
+        {
+            BD_RESERVAS_VUELOSEntities1 data = new BD_RESERVAS_VUELOSEntities1();
+           
+            return View();
+        }
+    }
+}
