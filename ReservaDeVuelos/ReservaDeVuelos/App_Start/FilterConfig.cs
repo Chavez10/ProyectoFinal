c3e1a6d8c6d1228c8693de54c740ac1c678dd3dc
david <david@DESKTOP-KSMVEUL>
2020-06-05 15:06:23 -0600

    FiltersConfiguration

--- ReservaDeVuelos/ReservaDeVuelos/App_Start/FilterConfig.cs
+++ ReservaDeVuelos/ReservaDeVuelos/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace ReservaDeVuelos
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filters.ValidarSesiones());
         }
     }
 }
