e204c5cc82a37835bbc6430f9fb9b900c880d19d
HP <HP@DESKTOP-TIIAHDB>
2020-06-05 15:46:49 -0600

    cambios en homecontroller

--- ReservaDeVuelos/ReservaDeVuelos/Controllers/HomeController.cs
+++ ReservaDeVuelos/ReservaDeVuelos/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReservaDeVuelos.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +9,19 @@ namespace ReservaDeVuelos.Controllers
 {
     public class HomeController : Controller
     {
+        [Autorizaciones(CodOperacion: 1)]
         public ActionResult Index()
         {
             return View();
         }
-
+        [Autorizaciones(CodOperacion: 1)]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [Autorizaciones(CodOperacion: 1)]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -28,7 +30,7 @@ namespace ReservaDeVuelos.Controllers
         }
 
         /* --- METODOS HA TRABAJAR DESDE AQUÍ --- */
-
+        [Autorizaciones (CodOperacion: 2)]
         public ActionResult Dashboard()
         {
             return View();
