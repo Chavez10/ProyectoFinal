9b7336c67d7215e12685e2ca84109bcd8b3ed6b6
HP <HP@DESKTOP-TIIAHDB>
2020-06-05 16:27:00 -0600

    Listar Usuarios en proceso

---
+++ ReservaDeVuelos/ReservaDeVuelos/Models/VistaUsuariosModel/InfoUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReservaDeVuelos.Models.VistaUsuariosModel
+{
+    public class InfoUser
+    {
+
+    }
+}
