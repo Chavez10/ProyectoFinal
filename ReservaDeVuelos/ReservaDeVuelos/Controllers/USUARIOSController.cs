febdfb916878916e27ee174c65328bae89ae701f
HP <HP@DESKTOP-TIIAHDB>
2020-06-07 15:17:50 -0600

    cambios

---
+++ ReservaDeVuelos/ReservaDeVuelos/Controllers/USUARIOSController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReservaDeVuelos.Models;
+using ReservaDeVuelos.Models.VistaUsuariosModel;
+
+namespace ReservaDeVuelos.Controllers
+{
+    public class USUARIOSController : Controller
+    {
+        // GET: USUARIOS
+        public ActionResult Index()
+        {
+            List<InfoUser> lista = null;
+            using(BD_RESERVAS_VUELOSEntities2 data = new BD_RESERVAS_VUELOSEntities2())
+            {
+                lista = (from info in data.USUARIOS
+                         where info.ESTADO == true
+                         orderby info.EMAIL
+                         select new InfoUser
+                         {
+                             COD_USER = info.COD_USUARIO,
+                             NOM_USER = info.NOMBRES,
+                             MAIL_USER = info.EMAIL,
+                             COD_ROL = info.ROL,
+                             COD_ESTADO = info.ESTADO
+                         }).ToList();
+            }
+            return View(lista);
+        }
+    }
+}
--- ReservaDeVuelos/ReservaDeVuelos/Models/VistaUsuariosModel/InfoUser.cs
+++ ReservaDeVuelos/ReservaDeVuelos/Models/VistaUsuariosModel/InfoUser.cs
@@ -7,6 +7,10 @@ namespace ReservaDeVuelos.Models.VistaUsuariosModel
 {
     public class InfoUser
     {
-
+        public int COD_USER { get; set; }
+        public string NOM_USER { get; set; }
+        public string MAIL_USER { get; set; }
+        public int? COD_ROL { get; set; }
+        public bool COD_ESTADO { get; set; }
     }
 }
