fe60742c286342c43eb627841cbfa36a30591116
david <david@DESKTOP-KSMVEUL>
2020-06-14 23:59:58 -0600

    Se agrega parte del Dashboard a utilizar

--- ReservaDeVuelos/ReservaDeVuelos/Controllers/HomeController.cs
+++ ReservaDeVuelos/ReservaDeVuelos/Controllers/HomeController.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ReservaDeVuelos.Models;
 
 namespace ReservaDeVuelos.Controllers
 {
     public class HomeController : Controller
     {
+        private bdVuelosEntities1 data = new bdVuelosEntities1();
+
         [Autorizaciones(CodOperacion: 1)]
         public ActionResult Index()
         {
@@ -32,6 +35,22 @@ namespace ReservaDeVuelos.Controllers
         /* --- METODOS HA TRABAJAR DESDE AQUÍ --- */
         [Autorizaciones (CodOperacion: 5)]
         public ActionResult Dashboard()
+        {
+
+            
+            var list = (from ra in data.REGION_AEROPUERTO
+                    join ar in data.AEROPUERTOS on ra.AEROPUERTO equals ar.COD_AERO
+                    join r in data.REGIONES on ra.REGION equals r.COD_REGION
+                    orderby r.REGION ascending
+                    select ar.NOM_AERO +", "+ r.REGION
+                ).FirstOrDefault();
+
+            ViewBag.Origen = new SelectList(list, "COD_REG_AER");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertarReserva()
         {
             return View();
         }
