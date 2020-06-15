1d50e559cdd415fce3b9d703ab97fdde7f20d625
david <david@DESKTOP-KSMVEUL>
2020-06-15 15:49:35 -0600

    Dashboard DropDownList Origen y Destino

--- ReservaDeVuelos/ReservaDeVuelos/Controllers/HomeController.cs
+++ ReservaDeVuelos/ReservaDeVuelos/Controllers/HomeController.cs
@@ -36,21 +36,35 @@ namespace ReservaDeVuelos.Controllers
         [Autorizaciones (CodOperacion: 5)]
         public ActionResult Dashboard()
         {
-
-            
             var list = (from ra in data.REGION_AEROPUERTO
                     join ar in data.AEROPUERTOS on ra.AEROPUERTO equals ar.COD_AERO
                     join r in data.REGIONES on ra.REGION equals r.COD_REGION
-                    orderby r.REGION ascending
-                    select ar.NOM_AERO +", "+ r.REGION
-                ).FirstOrDefault();
+                    orderby ar.NOM_AERO ascending
+                    select new
+                    {
+                        id = ra.COD_REG_AER,
+                        regiones = ar.NOM_AERO + ", " + r.REGION
+                    }
+                );
+            var aerolist = (from arP in data.AEROLINEA_AEROPUERTO
+                            join ra in data.REGION_AEROPUERTO on arP.REGION_AEROPUERTO equals ra.COD_REG_AER
+                            join psa in data.PAIS_AEROLINEA on arP.PAIS_AEROLINEA equals psa.COD_PAIS_AEROLINEA
+                            join arL in data.AEROLINEAS on psa.AEROLINEA equals arL.COD_AEROLINEA
+                            where arP.REGION_AEROPUERTO == ra.COD_REG_AER
+                            select new
+                            {
+                                id = arP.COD_AL_AP
+                            }
+                );
+            
 
-            ViewBag.Origen = new SelectList(list, "COD_REG_AER");
+            ViewBag.Origen = new SelectList(list, "id", "regiones");
+            ViewBag.Destino = new SelectList(list, "id", "regiones");
             return View();
         }
 
         [HttpPost]
-        public ActionResult InsertarReserva()
+        public ActionResult InsertarReserva(int id)
         {
             return View();
         }
