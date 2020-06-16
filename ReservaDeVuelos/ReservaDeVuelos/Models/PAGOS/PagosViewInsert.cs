5cb4c266767a9e79c3a71b9c19063461caf3382d
HP <HP@DESKTOP-TIIAHDB>
2020-06-15 22:27:45 -0600

    pagos pendiente

---
+++ ReservaDeVuelos/ReservaDeVuelos/Models/PAGOS/PagosViewInsert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.WebPages.Html;
+using System.Web.Mvc;
+
+namespace ReservaDeVuelos.Models.PAGOS
+{
+    public class PagosViewInsert
+    {
+        public int COD_PROC { get; set; }
+        public int COD_REV { get; set; }
+        public int COD_TP { get; set; }
+        public int COD_PR { get; set; }
+        [Required]
+        [Display(Name = "Monto Total")]
+        public Decimal MONTO { get; set; }
+        
+
+    }
+}
